8abf5697f8119a92530abbc310be33075b2caa6c
roatt-tilma <roshanbhattmnr@gmail.com>
2021-05-24 22:48:14 +0545

    make a simple smart contract and set up project to write clientside code

--- contracts/eVoting.sol
+++ contracts/eVoting.sol
@@ -1,5 +1,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract eVoting{
-    uint public voteCount = 0;
+    uint public totalVoteCount = 0;
+    uint public candidateCount = 0;
+
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public Candidates;
+
+    constructor() public{
+        createCandidate("Roshan Bhatt");
+    }
+
+    function createCandidate(string memory _name) public{
+        candidateCount ++;
+        Candidates[candidateCount] = Candidate(candidateCount,_name,0);
+    }
 }
