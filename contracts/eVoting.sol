7edb49a1b496997e652a347facac6e7998252bb4
rrithick116 <rrithick116@gmail.com>
2021-05-28 21:01:39 +0530

    Blockchain connection using web3js

--- contracts/eVoting.sol
+++ contracts/eVoting.sol
@@ -1,23 +1,73 @@
 pragma solidity >=0.4.22 <0.9.0;
+ 
 
 contract eVoting{
-    uint public totalVoteCount = 0;
-    uint public candidateCount = 0;
-
-    struct Candidate{
+ struct Candidate{
         uint id;
         string name;
         uint voteCount;
     }
-
-    constructor() public{
-        createCandidate("Roshan Bhatt");
+    
+    struct Voter{
+        //bool authorized;
+        bool voted;
+        uint vote;
+    }
+    
+    address payable public owner;
+    string public electionName;
+    uint public noOfCandidates;
+    
+    mapping(address=>Voter) public voters;
+    Candidate[] public candidates;
+    
+    uint public totalVotes;
+    
+    constructor(string memory _name) public{
+        owner=msg.sender;
+        electionName=_name;
+        addCandidate("rithick");
+        addCandidate("roshan");
+        addCandidate("roshan bhatt");
     }
 
-    mapping(uint => Candidate) public Candidates;
-
-    function createCandidate(string memory _name) public{
-        candidateCount ++;
-        Candidates[candidateCount] = Candidate(candidateCount,_name,0);
+     event votedEvent (
+        uint indexed _voteIndex
+    );
+    
+    modifier ownerOnly(){
+        require(owner==msg.sender);
+        _;
+    }
+    
+    function addCandidate(string memory _name)ownerOnly public{
+        noOfCandidates++;
+        candidates.push(Candidate(noOfCandidates,_name,0));
+    }
+    
+    // function getNumCandidate() public view returns(uint){
+    //     return candidates.length;
+    // }
+    
+    // function authorize(address _person) ownerOnly public{
+    //     voters[_person].authorized=true;
+    // }
+    
+    function vote(uint _voteIndex) public{
+        require(!voters[msg.sender].voted);
+        //require(voters[msg.sender].authorized);
+        
+        voters[msg.sender].vote=_voteIndex;
+        voters[msg.sender].voted=true;
+        
+        candidates[_voteIndex-1].voteCount+=1;
+        totalVotes+=1;
+        
+        emit votedEvent(_voteIndex);
+        
+    }
+    
+    function end() public ownerOnly{
+        selfdestruct(owner);
     }
 }
---
+++ src/app.js
@@ -0,0 +1,152 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  hasVoted: false,
+
+  init: function() {
+    return App.loadWeb3();
+  },
+
+
+
+ loadWeb3: async () => {
+      if (typeof web3 !== 'undefined') {
+        App.web3Provider = web3.currentProvider
+        web3 = new Web3(web3.currentProvider)
+      } else {
+        window.alert("Please connect to Metamask.")
+      }
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        window.web3 = new Web3(ethereum)
+        try {
+          // Request account access if needed
+          await ethereum.enable()
+          // Acccounts now exposed
+          web3.eth.sendTransaction({/* ... */})
+        } catch (error) {
+          
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        App.web3Provider = web3.currentProvider
+        window.web3 = new Web3(web3.currentProvider)
+        // Acccounts always exposed
+        web3.eth.sendTransaction({/* ... */})
+      }
+      // Non-dapp browsers...
+      else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
+      return App.initContract();
+    },
+
+  initContract: function() {
+    $.getJSON("eVoting.json", function(election) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.eVoting = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.eVoting.setProvider(App.web3Provider);
+
+      App.listenForEvents();
+
+      return App.render();
+    });
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.eVoting.deployed().then(function(instance) {
+      
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
+    });
+  },
+
+  render: function() {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    // Load contract data
+    App.contracts.eVoting.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.noOfCandidates();
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
+        });
+      }
+      return electionInstance.voters(App.account)[0];
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+  },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.eVoting.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+
+  endElection: function(){
+    console.log("ending  election");
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
